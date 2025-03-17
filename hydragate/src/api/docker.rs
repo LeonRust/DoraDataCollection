f103b204a6c8034242a58ec129c3f6445affbb79
EchoAI <Echo_AI@foxmail.com>
2025-03-18 05:59:18 +0800

    feta: docker

--- hydragate/src/api/docker.rs
+++ hydragate/src/api/docker.rs
@@ -25,14 +25,14 @@ async fn run(Extension(db_state): Extension<Arc<DbState>>) -> Result<impl IntoRe
 
     let mut u2d2_left = None; // 摇操臂左
     let mut u2d2_right = None; // 摇操臂右
-    // let mut orbbec_head = None; // 头部相机
-    // let mut orbbec_left = None; // 左侧相机
-    // let mut orbbec_right = None; // 右侧相机
+    let mut orbbec_head = None; // 头部相机
+    let mut orbbec_left = None; // 左侧相机
+    let mut orbbec_right = None; // 右侧相机
 
     let u2d2_serials = util::find_usb_driver(UsbType::U2D2);
     let u2d2_usb_devices = util::find_usb_number(UsbType::U2D2, &u2d2_serials);
-    // let orbbec_serials = util::find_usb_driver(UsbType::Orbbec);
-    // let orbbec_usb_devices = util::find_usb_number(UsbType::Orbbec, &orbbec_serials);
+    let orbbec_serials = util::find_usb_driver(UsbType::Orbbec);
+    let orbbec_usb_devices = util::find_usb_number(UsbType::Orbbec, &orbbec_serials);
 
     for setting in settings.iter() {
         match setting.device_type {
@@ -56,24 +56,24 @@ async fn run(Extension(db_state): Extension<Arc<DbState>>) -> Result<impl IntoRe
             }
             2 => {
                 // 奥比中光
-                // let Some(value) = orbbec_usb_devices.get(&setting.device_serial) else {
-                //     return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
-                // };
-                // match setting.device_place {
-                //     1 => {
-                //         // 头部
-                //         orbbec_head = Some((setting.device_serial.as_str(), value.as_str()));
-                //     }
-                //     2 => {
-                //         // 左侧
-                //         orbbec_left = Some((setting.device_serial.as_str(), value.as_str()));
-                //     }
-                //     3 => {
-                //         // 右侧
-                //         orbbec_right = Some((setting.device_serial.as_str(), value.as_str()));
-                //     }
-                //     _ => {}
-                // }
+                let Some(value) = orbbec_usb_devices.get(&setting.device_serial) else {
+                    return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
+                };
+                match setting.device_place {
+                    1 => {
+                        // 头部
+                        orbbec_head = Some((setting.device_serial.as_str(), value.as_str()));
+                    }
+                    2 => {
+                        // 左侧
+                        orbbec_left = Some((setting.device_serial.as_str(), value.as_str()));
+                    }
+                    3 => {
+                        // 右侧
+                        orbbec_right = Some((setting.device_serial.as_str(), value.as_str()));
+                    }
+                    _ => {}
+                }
             }
             _ => {}
         }
@@ -85,18 +85,18 @@ async fn run(Extension(db_state): Extension<Arc<DbState>>) -> Result<impl IntoRe
     let Some(u2d2_right) = u2d2_right else {
         return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
     };
-    // let Some(orbbec_head) = orbbec_head else {
-    //     return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
-    // };
-    // let Some(orbbec_left) = orbbec_left else {
-    //     return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
-    // };
-    // let Some(orbbec_right) = orbbec_right else {
-    //     return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
-    // };
+    let Some(orbbec_head) = orbbec_head else {
+        return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
+    };
+    let Some(orbbec_left) = orbbec_left else {
+        return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
+    };
+    let Some(orbbec_right) = orbbec_right else {
+        return Err(Error::App(AppError::RUN_SYSTEM_FAIL));
+    };
 
     // 启动lerobot-gen72
-    let a = Command::new("sudo")
+    Command::new("sudo")
         .args([
             "docker",
             "run",
@@ -127,8 +127,49 @@ async fn run(Extension(db_state): Extension<Arc<DbState>>) -> Result<impl IntoRe
             format!("{}:/lerobot-gen72/.cache", db_state.cache_path).as_str(),
             "lerobot-gen72",
         ])
-        .output();
-    dbg!(a);
+        .output()
+        .ok();
+
+    // 启动 head
+    for (index, &(name, camera)) in [orbbec_head, orbbec_left, orbbec_right].iter().enumerate() {
+        Command::new("sudo")
+            .args([
+                "docker",
+                "run",
+                "--rm",
+                "-d",
+                "--name",
+                format!(
+                    "camera-{}-{}",
+                    if index == 0 {
+                        "head"
+                    } else if index == 1 {
+                        "left"
+                    } else {
+                        "right"
+                    },
+                    name
+                )
+                .as_str(),
+                "--network",
+                "host",
+                "--device",
+                format!("/dev/{}:/dev/video0", camera).as_str(),
+                "-e",
+                "DAEMON_IP=127.0.0.1",
+                "-e",
+                "DAEMON_TCP_PORT=1234",
+                "-e",
+                "CAMERA_ID=0",
+                "-e",
+                format!("CAMERA_NUMBER={}", index + 1).as_str(),
+                "-v",
+                format!("{}:/datasets", db_state.datasets_path).as_str(),
+                "camera",
+            ])
+            .output()
+            .ok();
+    }
 
     Ok(Json(ApiResult::OK))
 }
